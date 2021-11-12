43da6d85c892090590dee6c94261d808920d66d2
Ephraim Atta-Duncan <ephraimduncan68@gmail.com>
2021-11-12 12:59:05 +0000

    feat: add note button

--- pages/index.js
+++ pages/index.js
@@ -1,7 +1,24 @@
 import Head from "next/head";
 import styles from "../styles/Home.module.css";
-import { blue, indigo, tomato } from "@radix-ui/colors";
+import { blue, indigo, red, tomato } from "@radix-ui/colors";
 import { useLocalStorage } from "../hooks/useLocalStorage";
+
+const PlusComponent = () => (
+  <svg
+    width="15"
+    height="15"
+    viewBox="0 0 15 15"
+    fill="none"
+    xmlns="http://www.w3.org/2000/svg"
+  >
+    <path
+      d="M8 2.75C8 2.47386 7.77614 2.25 7.5 2.25C7.22386 2.25 7 2.47386 7 2.75V7H2.75C2.47386 7 2.25 7.22386 2.25 7.5C2.25 7.77614 2.47386 8 2.75 8H7V12.25C7 12.5261 7.22386 12.75 7.5 12.75C7.77614 12.75 8 12.5261 8 12.25V8H12.25C12.5261 8 12.75 7.77614 12.75 7.5C12.75 7.22386 12.5261 7 12.25 7H8V2.75Z"
+      fill="currentColor"
+      fill-rule="evenodd"
+      clip-rule="evenodd"
+    ></path>
+  </svg>
+);
 
 const commonStyle = {
   padding: "2rem",
@@ -27,7 +44,7 @@ const findArray = (array, key) => array.find((note) => note.key === key);
 
 function truncate(str, maxlength) {
   return +str.length > maxlength
-    ? "📍 " + str.slice(0, maxlength) + "..."
+    ? "📍 " + str.slice(0, maxlength).trim() + "..."
     : "📍 " + str;
 }
 
@@ -50,7 +67,7 @@ export default function Home() {
   const [note, setNote] = useLocalStorage("note", "");
 
   // const note = {
-  //   key: "random-generated-key",
+  //   id: number,
   //   text: "note-text",
   // };
 
@@ -95,6 +112,10 @@ export default function Home() {
               autoFocus
             />
             <pre aria-hidden>{note}</pre>
+
+            <button className={styles.addButton}>
+              <PlusComponent />
+            </button>
           </div>
         </div>
       </main>
