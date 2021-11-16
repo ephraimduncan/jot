0a749634a2302e14950c1eba2ad520391150f5a4
Ephraim Atta-Duncan <ephraimduncan68@gmail.com>
2021-11-16 18:19:38 +0000

    chore: add extension icons

--- bundle.sh
+++ bundle.sh
@@ -6,6 +6,12 @@ yarn next export;
 
 cp ./manifest.json ./out;
 
+cp ./public/extension-icons/icon16.png ./out
+cp ./public/extension-icons/icon48.png ./out
+cp ./public/extension-icons/icon128.png ./out
+
+rm -rf ./out/extension-icons
+
 node utils/postBuild.js
 
 cd out && zip -r -FS ../jot-tab-notes.zip *;
